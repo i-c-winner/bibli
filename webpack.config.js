10563f9a808c56897d4f5a1e109c22e2af0072e9
Дмитрий Белоусов <d-belousov@hotmail.com>
2020-10-07 00:43:58 +0300

    под загрузку

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
         template: './src/piccolo/index.html',
-        filename: 'main.html',      
+        filename: 'index.html',      
     })
   ]
 };
